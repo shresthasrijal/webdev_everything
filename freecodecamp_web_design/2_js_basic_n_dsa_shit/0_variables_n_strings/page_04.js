52e87504c2f221f730f0b8dea8348d99c8a2ec69
Srijal Shrestha <srijaldhar@gmail.com>
2026-06-01 17:00:15 +0545

    blah blah blacksheep

---
+++ freecodecamp_web_design/2_js_basic_n_dsa_shit/0_variables_n_strings/page_04.js
@@ -0,0 +1,26 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "#1 facts bot";
+let botLocation = "U.S. of A";
+let favoriteLanguage = "Rust";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Your Mom Gay!" + "Because she likes" + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = "Your dad lesbian" + "Because he likes" + favoriteLanguage + ".";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is ass fr fr fr!";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);
---
+++ freecodecamp_web_design/2_js_basic_n_dsa_shit/0_variables_n_strings/page_05.js
@@ -0,0 +1,21 @@
+let adjective = "happy";
+let noun = "dog";
+let verb = "run";
+let place = "park";
+let adjective2 = "sunny";
+let noun2 = "ball";
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("First story: " + firstStory);
+
+adjective = "mysterious";
+noun = "dragon";
+verb = "sleeping";
+place = "cave";
+adjective2 = "sparkling";
+noun2 = "marshmallows";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
