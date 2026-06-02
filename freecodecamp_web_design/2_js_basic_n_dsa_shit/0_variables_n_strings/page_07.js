ec57e68349bb07c749c79a3678c5041b016c41aa
Srijal Shrestha <srijaldhar@gmail.com>
2026-06-02 14:35:31 +0545

    js redo

---
+++ freecodecamp_web_design/2_js_basic_n_dsa_shit/0_variables_n_strings/page_07.js
@@ -0,0 +1,22 @@
+console.log("Hi there!");
+
+const botName = "teacherBot";
+
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+const subject = "JavaScript";
+const topic = "strings";
+console.log("");
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
+
+let strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+console.log(strLengthIntro);
+
+let length = console.log(subject.length);
+
+let step8 = `Here is an example of using the length property on the word ${topic}.`;
+console.log(step8);
+console.log(step8.length);
