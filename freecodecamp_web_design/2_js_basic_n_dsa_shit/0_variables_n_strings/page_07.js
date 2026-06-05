825e9593f065b25131c6c6d2305abb644534ac60
Srijal Shrestha <srijaldhar@gmail.com>
2026-06-05 11:20:52 +0545

    String methods

--- freecodecamp_web_design/2_js_basic_n_dsa_shit/0_variables_n_strings/page_07.js
+++ freecodecamp_web_design/2_js_basic_n_dsa_shit/0_variables_n_strings/page_07.js
@@ -7,16 +7,48 @@ console.log(greeting);
 
 const subject = "JavaScript";
 const topic = "strings";
-console.log("");
 
 const sentence = `Today, you will learn about ${topic} in ${subject}.`;
 console.log(sentence);
 
-let strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
+const strLengthIntro = `Here is an example of using the length property on the word ${subject}.`;
 console.log(strLengthIntro);
 
-let length = console.log(subject.length);
+console.log(subject.length);
 
-let step8 = `Here is an example of using the length property on the word ${topic}.`;
-console.log(step8);
-console.log(step8.length);
+console.log(
+  `Here is an example of using the length property on the word ${topic}.`,
+);
+console.log(topic.length);
+
+console.log(
+  `Here is an example of accessing the first letter in the word ${subject}.`,
+);
+
+console.log(subject[0]);
+
+console.log(
+  `Here is an example of accessing the second letter in the word ${subject}.`,
+);
+console.log(subject[1]);
+
+console.log(
+  `Here is an example of accessing the last letter in the word ${subject}.`,
+);
+
+const lastCharacter = subject[subject.length - 1];
+console.log(lastCharacter);
+
+const learningIsFunSentence = "Learning is fun.";
+
+console.log(
+  "Here are examples of finding the positions of substrings in the sentence.",
+);
+
+console.log(learningIsFunSentence.indexOf("Learning"));
+
+console.log(learningIsFunSentence.indexOf("fun"));
+
+console.log(learningIsFunSentence.indexOf("learning"));
+
+console.log("I hope you enjoyed learning today.");
